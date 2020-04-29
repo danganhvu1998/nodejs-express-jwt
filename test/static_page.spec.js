93ef8974a3913033518ae535a3a82190fb7a84d8
danganhvu1998 <danganhvu1998@gmail.com>
2020-04-29 10:26:08 +0700

    add test /user

--- test/static_page.spec.js
+++ test/static_page.spec.js
@@ -8,3 +8,12 @@ test('get root url', async () => {
     expect(response.statusCode).toBe(200);
 })
 
+describe("Test /user", () => {
+    test('get /user', async () => {
+        const response = await request(App).get("/user");
+        expect( JSON.parse(response.res.text).response ).toEqual( 
+            expect.stringMatching(/^Hello World/)
+        )
+        expect(response.statusCode).toBe(200);
+    })
+})
